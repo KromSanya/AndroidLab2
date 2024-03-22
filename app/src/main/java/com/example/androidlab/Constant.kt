3e22e2286e212d9800d1312c46a36df576675489
Alexandr <kromval6@gmail.com>
2024-03-22 16:53:11 +0600

    Add Constant
    (cherry picked from commit 4b0b19d3c0b46138ba30e70dec50d499a37c262b)

--- app/src/main/java/com/example/androidlab/Constant.kt
+++ app/src/main/java/com/example/androidlab/Constant.kt
@@ -2,39 +2,27 @@ package com.example.androidlab
 
 import androidx.compose.ui.unit.sp
 
-data class Hero(
-    val imageUrl: Int, val name: String, val phrase: String
-)
-
 object Constant {
-
-    const val HomeScreen = "FirstScreen"
-    const val DetailScreen = "SecondScreen"
-    const val ChooseHero = "Choose your hero"
+    val HomeScreen = "FirstScreen"
+    val DetailScreen = "SecondScreen"
+    val ChooseHero = "Choose your hero"
 
     val averFont = 28.sp
     val bigFont = 36.sp
 
-    val deadpool = Hero(
+    val imageUrls = listOf(
         R.drawable.deadpool,
-        "DeadPool",
-        "Please don’t make the supersuit green...or animated!"
-    )
-    val ironman = Hero(
         R.drawable.ironman,
-        "Iron Man",
-        "You know who I am"
+        R.drawable.spider
     )
-    val spiderman = Hero(
-        R.drawable.spider,
-        "Spider Man",
+    val names = listOf(
+        "DeadPool",
+        "Iron Man",
+        "Spider Man"
+    )
+    val heroesPhrase = listOf(
+        "Please don’t make the supersuit green...or animated!",
+        "You know who I'am",
         "In iron suit"
     )
-
-    val heroes = listOf(
-        deadpool,
-        ironman,
-        spiderman
-    )
-    val size = heroes.size
 }
